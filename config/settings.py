275ab66565288735be01df3890d6335891347f7e
nunes-willi <nuneswillroberto@gmail.com>
2023-10-24 15:15:55 -0300

    Fl0

--- config/settings.py
+++ config/settings.py
@@ -62,6 +62,8 @@ MIDDLEWARE = [
     "corsheaders.middleware.CorsMiddleware",
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
+    'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
@@ -165,3 +167,7 @@ REST_FRAMEWORK = {
 }
 
 AUTH_USER_MODEL = "usuario.Usuario"
+
+if MODE == "PRODUCTION":
+    STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+    STATICFILES_STORAGE = "whitenoise.storage.CompressedManifestStaticFilesStorage"
